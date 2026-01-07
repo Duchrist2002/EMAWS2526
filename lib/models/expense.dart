0750c8c815322388251f5e716f9a7ed844bff811
Duchrist <153777772+Duchrist2002@users.noreply.github.com>
2026-01-07 15:34:18 +0100

    export expense

---
+++ lib/models/expense.dart
@@ -0,0 +1,16 @@
+
+class Expense {
+  final String title;
+  final String category;
+  final double amount;
+  final DateTime date;
+  final String? note;
+
+  Expense({
+    required this.title,
+    required this.category,
+    required this.amount,
+    required this.date,
+    this.note,
+  });
+}
